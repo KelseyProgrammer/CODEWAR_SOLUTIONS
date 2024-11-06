87e8372c93e5cd48e1a8c5b5d2945c026c6af4a4
Christopher Ament <chrisament45@gmail.com>
2024-11-06 12:12:43 -0500

    74: Grasshopper - Terminal game combat function - lvl 8

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// 74: Grasshopper - Terminal game combat function - lvl 8
+// Description:
+// Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
+function combat(health, damage) {
+  let newHealth = health - damage
+ if(newHealth > 0){
+   return newHealth
+ }else {
+   return 0
+ }
+ }
 // 73: Total amount of points - lvl 8
 // Description:
 // Our football team has finished the championship.
