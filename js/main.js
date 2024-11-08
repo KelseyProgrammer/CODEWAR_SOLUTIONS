1d0b2f364fd8a59e2f00cc51f7633d35eeb0af11
Christopher Ament <chrisament45@gmail.com>
2024-11-07 19:52:08 -0500

    added 75: Is he gonna survive? - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// 75: Is he gonna survive? - lvl 8
+// Description:
+// A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+// 
+// Return true if yes, false otherwise :)
+function hero(bullets, dragons){
+  return bullets / dragons >= 2 ? true : false
+  }
+
+
 // 74: Grasshopper - Terminal game combat function - lvl 8
 // Description:
 // Create a combat function that takes the player's current health and the amount of damage received, and returns the player's new health. Health can't be less than 0.
