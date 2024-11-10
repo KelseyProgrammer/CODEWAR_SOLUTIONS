b34665486c6bfc3acaaf50119da127b95bb1ac58
Christopher Ament <chrisament45@gmail.com>
2024-11-09 20:06:19 -0500

    added // 77: Calculate BMI - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+// 77: Calculate BMI - lvl 8
+// Description:
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+// 
+// if bmi <= 18.5 return "Underweight"
+// 
+// if bmi <= 25.0 return "Normal"
+// 
+// if bmi <= 30.0 return "Overweight"
+// 
+// if bmi > 30 return "Obese"
+function bmi(weight, height) {
+  let bodyMass = weight / (height ** 2);
+if(bodyMass <= 18.5 ){
+  return "Underweight";
+}else if (bodyMass <= 25 ){
+   return "Normal";
+  }else if (bodyMass <= 30) {
+return "Overweight";
+    }else {
+return "Obese";
+      }
+}
 // 76: Bouncing Balls - lvl 6
 // A child is playing with a ball on the nth floor of a tall building. The height of this floor above ground level, h, is known.
 // 
