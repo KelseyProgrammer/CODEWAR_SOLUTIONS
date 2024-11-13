da15b6b1906b75a6404eec87f53c3746f45d87de
Christopher Ament <chrisament45@gmail.com>
2024-11-13 10:32:51 -0500

    added  81: Count of positives / sum of negatives - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+// 81: Count of positives / sum of negatives - lvl 8
+// Description:
+// Given an array of integers.
+// 
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+// 
+// If the input is an empty array or is null, return an empty array.
+// 
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+function countPositivesSumNegatives(input) {
+  if (input == null || input.length == 0)
+    return [];
+  
+  var positive = 0;
+  var negative = 0;
+  
+  for (var i=0, l=input.length; i<l; ++i)
+  {
+    if (input[i] > 0)
+      ++ positive;
+    else
+      negative += input[i];
+  }
+  
+  return [positive, negative];
+}
 // 80: Binary Solution - lvl 7
 // Description:
 // Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
