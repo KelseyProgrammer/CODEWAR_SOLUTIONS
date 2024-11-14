6c7ff49a7f9e1032aa75086decf8225bf1fea575
Christopher Ament <chrisament45@gmail.com>
2024-11-14 01:35:54 -0500

    added 82: Reversing Words in a String kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
+// 82: Reversing Words in a String
+// Description:
+// You need to write a function that reverses the words in a given string. Words are always separated by a single space.
+// 
+// As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+// 
+// Example (Input --> Output)
+// 
+// "Hello World" --> "World Hello"
+// "Hi There." --> "There. Hi"
+function reverse(string){
+  return string.split(' ').reverse().join(' ')
+ }
 // 81: Count of positives / sum of negatives - lvl 8
 // Description:
 // Given an array of integers.
