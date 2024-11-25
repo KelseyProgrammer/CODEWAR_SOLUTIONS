92487627d5593637d594ae9628a3cf14f5227fc9
Christopher Ament <chrisament45@gmail.com>
2024-11-25 12:41:02 -0500

    added 92: Friend or Foe - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// 92: Friend or Foe - lvl 7
+// Description:
+// Make a program that filters a list of strings and returns a list with only your friends name in it.
+// 
+// If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+// 
+// Input = ["Ryan", "Kieran", "Jason", "Yous"]
+// Output = ["Ryan", "Yous"]
+// 
+// Input = ["Peter", "Stephen", "Joe"]
+// Output = []
+// Input strings will only contain letters. Note: keep the original order of the names in the output.
+function friend(friends){
+  return friends.filter((friends)=> friends.length === 4);
+}
 // 91: Grasshopper - Messi Goals - lvl 8
 // Messi's Goal Total
 // Use variables to find the sum of the goals Messi scored in 3 competitions
