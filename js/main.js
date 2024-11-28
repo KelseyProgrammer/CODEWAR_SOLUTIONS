cd3c9875dedb2a6fb3459cffc736e883cf7f37bc
Christopher Ament <chrisament45@gmail.com>
2024-11-28 11:33:38 -0500

    added 95: Fix String Case - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
+// 95: Fix String Case - lvl 7
+// Description:
+// In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+// 
+// make as few changes as possible.
+// if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+// For example:
+// 
+// solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+// solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+// solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+// More examples in test cases. Good luck!
+// 
+// Please also try:
+// 
+// Simple time difference
+// 
+// Simple remove duplicates
+function solve(s) {
+  let upperCount = 0;
+  let lowerCount = 0;
+   
+   for(let i = 0; i < s.length; i++){
+     if (s[i] === s[i].toUpperCase()){
+       upperCount ++
+     } else if(s[i] === s[i].toLowerCase()){
+       lowerCount ++
+     }
+   }
+   return lowerCount >= upperCount ? s.toLowerCase() : s.toUpperCase();
+ }
+ 
+
 // 94: Find the position - lvl 8
 // Description:
 // When provided with a letter, return its position in the alphabet.
