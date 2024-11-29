4fdfd39d2565d81b5895beb4fd2cd22e65e7fbb0
Christopher Ament <chrisament45@gmail.com>
2024-11-29 17:29:06 -0500

    added 96: Round up to the next multiple of 5 - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// 96: Round up to the next multiple of 5 - lvl 7
+// Description:
+// Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+// 
+// Examples:
+// 
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+// 
+// You can assume that all inputs are valid integers.
+function roundToNext5(n){
+  return Math.ceil(n/5)*5;
+}
 // 95: Fix String Case - lvl 7
 // Description:
 // In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
