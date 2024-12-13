438711037c2778ccfb20f100e7c4dce2c5e68fca
Christopher Ament <chrisament45@gmail.com>
2024-12-12 22:16:58 -0500

    added 111: Odd or Even? - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+// 111: Odd or Even? - lvl 7
+// Description:
+// Task:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+// 
+// Give your answer as a string matching "odd" or "even".
+// 
+// If the input array is empty consider it as: [0] (array with a zero).
+// 
+// Examples:
+// Input: [0]
+// Output: "even"
+// 
+// Input: [0, 1, 4]
+// Output: "odd"
+// 
+// Input: [0, -1, -5]
+// Output: "even"
+// Have fun!
+function oddOrEven(array) {
+  const sum = array.reduce((acc, curr)=> acc +curr, 0);
+   return sum % 2 === 0 ? "even" : "odd";
+    };
 // 110: Training JS #5: Basic data types--Object - lvl 8
 // Description:
 // In javascript, Object is one of basic data types. Define an Object can use var obj=new Object() or var obj={}
