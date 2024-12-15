71131567c3bd5f17dfa1f17272c7b50efcb1bd8b
Christopher Ament <chrisament45@gmail.com>
2024-12-15 10:49:03 -0500

    added 115: String cleaning - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// 115: String cleaning - lvl 8
+// Description:
+// Your boss decided to save money by purchasing some cut-rate optical character recognition software for scanning in the text of old novels to your database. At first it seems to capture words okay, but you quickly notice that it throws in a lot of numbers at random places in the text.
+// 
+// Examples (input -> output)
+// '! !'                 -> '! !'
+// '123456789'           -> ''
+// 'This looks5 grea8t!' -> 'This looks great!'
+// Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
+function stringClean(s){
+  return s.replace(/\d/g, '');
+ }
 // 114: Welcome! - lvl 8
 // Description:
 // Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
