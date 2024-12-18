44807d18d50679f4a3e93451e772bcd0aa8436ad
Christopher Ament <chrisament45@gmail.com>
2024-12-18 12:54:04 -0500

    added 118: Reversed sequence - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// 118: Reversed sequence - lvl 8
+// Description:
+// Build a function that returns an array of integers from n to 1 where n>0.
+// 
+// Example : n=5 --> [5,4,3,2,1]
+const reverseSeq = n => {
+ let result = [];
+for (let i = n; i > 0; i--){
+  result.push(i)
+}
+  return result;
+}
 // 117: Flatten and sort an array - lvl 7
 // Description:
 // Challenge:
