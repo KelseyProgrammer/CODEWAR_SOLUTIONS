98257b54c4177b9f2e77d88217fc1ccfa935d186
Christopher Ament <chrisament45@gmail.com>
2024-12-26 19:19:56 -0500

    added 127: Coupon Code - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// 127: Coupon Code - lvl 7
+// Description:
+// Story
+// Your online store likes to give out coupons for special occasions. Some customers try to cheat the system by entering invalid codes or using expired coupons.
+// 
+// Task
+// Your mission:
+// Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
+// 
+// A coupon is no more valid on the day AFTER the expiration date. All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+// 
+// Examples:
+// checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
+// checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
+  if (enteredCode !== correctCode){
+     return false;
+  }   
+   
+   const current = new Date(currentDate);
+     const expiration = new Date(expirationDate);
+    
+    return current <= expiration;
+  }
 // 126: Mexican Wave - lvl 6
 // Description:
 // Introduction
