9509b6589bcdef66332cf3c94d7b19206499a5e0
Christopher Ament <chrisament45@gmail.com>
2024-12-29 19:28:37 -0500

    added 130: Convert to Binary - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// 130: Convert to Binary - lvl 8
+// Description:
+// Task Overview
+// Given a non-negative integer b, write a function which returns an integer d such that the binary representation of b is the same as the decimal representation of d.
+// 
+// Examples:
+// 
+// n = 1 should return 1
+// n = 5 should return 101
+// n = 11 should return 1011 
+let toBinary = n => +n.toString(2)
 // 129: I love you, a little , a lot, passionately ... not at all - lvl 8
 // Description:
 // Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each of the following phrases each time a petal was torn:
