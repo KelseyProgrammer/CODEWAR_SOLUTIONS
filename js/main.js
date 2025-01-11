964c0481126c226a78fb3ebc49df3c242dbaf1a0
Christopher Ament <chrisament45@gmail.com>
2025-01-11 10:31:56 -0500

    added 143 : Hello Name Or World - lvl 8 kata solution and REFACTORED

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// 143 : Hello Name Or World - lvl 8
+// Description:
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+// 
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+// 
+// Examples:
+// 
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+// * With `name` not given 
+  // or `name` = ""        => return "Hello, World!"
+// Fundamentals
+function hello(name) {
+  if (typeof name === "string" && name.trim().length > 0) {
+    let fixed = name.trim().charAt(0).toUpperCase() + name.trim().slice(1).toLowerCase();
+    return `Hello, ${fixed}!`;
+  } else {
+    return "Hello, World!";
+  }
+}
+// REFACTOR:
+const hello = name => 
+  `Hello, ${name ? (name[0].toUpperCase() + name.slice(1).toLowerCase()) : 'World'}!`  
 // 142: Surface Area and Volume of a Box - lvl 8
 // Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 function getSize(width, height, depth) {
