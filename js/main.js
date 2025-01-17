cbedc127928ee3a684c4768dfeb172822b4aca40
Christopher Ament <chrisament45@gmail.com>
2025-01-17 18:36:57 -0500

    added 149: Regular Ball Super Ball - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+// 149: Regular Ball Super Ball - lvl 8
+// Forks (2)
+// Discourse (135)
+// Translations
+// Fork|Collect|
+// How satisfied are you with this kata?VerySomewhatNone
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+// 
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+// 
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+// 
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+class Ball {
+  constructor(ballType = "regular") {
+    this.ballType = ballType;
+  }
+}
+
+
+const ball1 = new Ball();
+const ball2 = new Ball("super");
+
+console.log(ball1.ballType); 
+console.log(ball2.ballType); 
 // 148: Century From Year - lvl 8
 // Description:
 // Introduction
