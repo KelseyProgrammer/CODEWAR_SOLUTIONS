a97d3308dd660c4ca5cb6420602c5fad49b6a36a
Christopher Ament <chrisament45@gmail.com>
2025-01-24 11:04:59 -0500

    added 157: Training JS #7: if..else and ternary operator - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,51 @@
+// 157: Training JS #7: if..else and ternary operator - lvl 8
+// 
+// Description:
+// In JavaScript, if..else is the most basic conditional statement, it consists of three parts:condition, statement1, statement2, like this:if (condition) statementa
+// 
+// 
+// else           statementb
+// It means that if the condition is true, then execute the statementa, otherwise execute the statementb. If the statementa or statementb are more than one line, you need to add { and } at the head and tail of statements in JS, to keep the same indentation on Python and to put an end in Ruby where it indeed ends.
+// 
+// For example, if we want to judge whether a number is odd or even, we can write code like this:function oddEven(n){
+// 
+  // else            return "even number";
+  // if (n % 2 == 1) return "odd number";
+  // 
+// }
+// If there is more than one condition to judge, we can use the compound if...else statement. For example:
+// if (age < 16)      return "children"
+// function oldYoung(age){
+  // else               return "old man"
+  // else if (age < 50) return "young man"   //use "else if" if needed
+  // 
+// }
+// This function returns a different value depending on the parameter age.
+// 
+// Looks very complicated? Well, JS and Ruby also support the ternary operator and Python has something similar too:
+// 
+// condition ? statementa : statementb
+// Condition and statement separated by "?", different statement separated by ":" in both Ruby and JS; in Python you put the condition in the middle of two alternatives. The two examples above can be simplified with ternary operator:
+// return n%2 == 1 ? "odd number" : "even number";
+// function oddEven(n){
+  // function oldYoung(age){
+// }
+// }
+  // return age < 16 ? "children" : age < 50 ? "young man" : "old man";
+// 
+// Task:
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.number of hotdogs	price per unit (cents)
+// 
+// n >= 5 and n < 10	95
+// n < 5	100
+// 
+// n >= 10	90
+// You can use if..else or ternary operator to complete it.
+// 
+// When you have finished the work, click "Run Tests" to see if your code is working properly.In the end, click "Submit" to submit your code and pass this kata.
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n < 10 ? n * 95 : n * 90;
+}
 // 156: How many stairs will Suzuki climb in 20 years? - lvl 8
 // Suzuki is a monk who climbs a large staircase to the monastery as part of a ritual. Some days he climbs more stairs than others depending on the number of students he must train in the morning. He is curious how many stairs might be climbed over the next 20 years and has spent a year marking down his daily progress.
 // 
