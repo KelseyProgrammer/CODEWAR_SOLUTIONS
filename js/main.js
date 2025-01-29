db2063800ca33fbabaa671d4d4b6085de6af7aa0
Christopher Ament <chrisament45@gmail.com>
2025-01-29 11:19:52 -0500

    added 161: USD => CNY - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// 161:USD => CNY - lvl 8
+//  Description:
+// Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
+// 
+// Examples (Input -> Output)
+// 15  -> '101.25 Chinese Yuan'
+// 465 -> '3138.75 Chinese Yuan'
+// The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
+function usdcny(usd) {
+  let yuan = usd * 6.75
+  let rounded = yuan.toFixed(2);
+  return `${rounded} Chinese Yuan`;
+}
+// REFACTOR:
+const usdcny = usd => `${(usd * 6.75).toFixed(2)} Chinese Yuan` 
 // 160: Do you speak "English"? - lvl 8
 // Description:
 // Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
