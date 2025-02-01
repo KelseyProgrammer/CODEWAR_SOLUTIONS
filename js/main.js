9c780676a0d3c6a87f07f262b0e0001f39f62379
Christopher Ament <chrisament45@gmail.com>
2025-01-31 23:00:19 -0500

    added 163: Convert a string to an array - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+// 163: Convert a string to an array - lvl 8
+// Description:
+// Write a function to split a string and convert it into an array of words.
+// 
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+// 
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+function stringToArray(string){
+  return string.split(' ');
+}
 // 162: Stringy Strings - lvl 8
 // Description:
 // write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
