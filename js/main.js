fb07d022a607f6752271652979f3ab86655f3b13
Christopher Ament <chrisament45@gmail.com>
2025-02-06 19:31:43 -0500

    added 167: Complementary DNA - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// 167: Complementary DNA - lvl 7
+// Description:
+// Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
+// 
+// If you want to know more: http://en.wikipedia.org/wiki/DNA
+// 
+// In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+// 
+// More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+function dnaStrand(dna) {
+  return dna.split('').map(str => {
+    if (str === "A") return "T";
+    if (str === "T") return "A";
+    if (str === "C") return "G";
+    if (str === "G") return "C";
+  }).join('');
+}
 // 166: JavaScript Array Filter - lvl 7
 // Description:
 // JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
