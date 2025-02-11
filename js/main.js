3c20e479f8105aea2eb1a1447ca245f41764ed96
Christopher Ament <chrisament45@gmail.com>
2025-02-11 14:16:21 -0500

    added  172: Exes and Ohs - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,4 +1,23 @@
-// 171: Find the Product - lvl 7
+// 172: Exes and Ohs - lvl 7
+// Description:
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+// 
+// Examples input/output:
+// 
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+function XO(str) {
+  str = str.toLowerCase();
+ 
+ const countX = (str.match(/x/g) || []).length;
+ const countO = (str.match(/o/g) || []).length;
+  
+ return countX === countO;
+}
+// 171: Find the Vowels - lvl 7
 // 
 // Description:
 // We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
