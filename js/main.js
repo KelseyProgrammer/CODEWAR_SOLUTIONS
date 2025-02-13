44a70a56bca096da2a632e9963dd5f5ea4edd6d5
Christopher Ament <chrisament45@gmail.com>
2025-02-13 13:19:34 -0500

    added 174: How old will I be in 2099? - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+// 174: How old will I be in 2099? - lvl 8
+// Description:
+// Philip's just turned four and he wants to know how old he will be in various years in the future such as 2090 or 3044. His parents can't keep up calculating this so they've begged you to help them out by writing a programme that can answer Philip's endless questions.
+// 
+// Your task is to write a function that takes two parameters: the year of birth and the year to count years in relation to. As Philip is getting more curious every day he may soon want to know how many years it was until he would be born, so your function needs to work with both dates in the future and in the past.
+// 
+// Provide output in this format: For dates in the future: "You are ... year(s) old." For dates in the past: "You will be born in ... year(s)." If the year of birth equals the year requested return: "You were born this very year!"
+// 
+// "..." are to be replaced by the number, followed and proceeded by a single space. Mind that you need to account for both "year" and "years", depending on the result.Good Luck!
+function calculateAge(bornYear, countYear) {  
+  let answer = countYear - bornYear;
+  
+  if (answer > 0) {
+    return `You are ${answer} ${answer === 1 ? "year" : "years"} old.`;
+  }
+  
+  if (answer === 0) {
+    return "You were born this very year!";
+  }
+  
+  if (answer < 0) {
+    return `You will be born in ${Math.abs(answer)} ${Math.abs(answer) === 1 ? "year" : "years"}.`;
+  }
+}
 // 173: Formatting decimal places #0 - lvl 8
 // Description:
 // Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
