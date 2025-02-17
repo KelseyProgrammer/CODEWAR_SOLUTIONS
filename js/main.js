57a5635f8a8c039b6469888b438e194fcac02248
Christopher Ament <chrisament45@gmail.com>
2025-02-16 19:46:06 -0500

    added 177: Simple validation of a username with regex - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// 177: Simple validation of a username with regex - lvl 8
+// Description:
+// Write a simple regex to validate a username. Allowed characters are:
+// 
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
 // 176: Determine offspring sex based on genes XX and XY chromosomes - lvl 8
 // 
 // Description:
