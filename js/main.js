29d3b361ca65198181bccddeedcfdc0815108d03
Christopher Ament <chrisament45@gmail.com>
2025-03-29 22:47:23 -0400

    added 204: Who likes it - lvl 6 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+// 204: Who likes it - lvl 6
+// Description:
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+// 
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+// 
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  }
+}
 // 203 : Multiply the number - lvl 8
 // Description:
 // Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
