b567c90732cce47d82c317bd892174282a2eea3c
Christopher Ament <chrisament45@gmail.com>
2025-05-24 21:20:42 -0400

    added 234: Playing with digits - lvl 6 kata solution

--- .history/js/main_20250524211949.js
+++ .history/js/main_20250524211949.js
@@ -6,18 +6,7 @@
 // 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
 // 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 // Given two positive integers n and p, we want to find a positive integer k, if it exists, such that the sum of the digits of n raised to consecutive powers starting from p is equal to k * n.In other words, writing the consecutive digits of n as a, b, c, d ..., is there an integer k such that :(ap+bp+1+cp+2+dp+3+...)=n∗k(a p +b p+1 +c p+2 +d p+3 +...)=n∗kIf it is the case we will return k, if not return -1.Note: n and p will always be strictly positive integers
-function digPow(n, p) {
-  const digits = String(n).split('').map(Number);
-  let sum = 0;
-  
-  for (let i = 0; i < digits.length; i++) {
-    sum += Math.pow(digits[i], p + i);
-  }
-  
-  const k = sum / n;
-  
-  return k % 1 === 0 && k > 0 ? k : -1;
-}
+
 // 233: Tidy Number (Special Numbers Series #9) - lvl 7
 // Definition
 // A Tidy Number is a number whose digits are in non-decreasing order.
