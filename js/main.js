64a007afc08a72f12d8fb28840ad57de3e7c3714
Christopher Ament <chrisament45@gmail.com>
2025-05-29 21:50:53 -0400

    added 237 Enumerable Magic #1 - True for All?- lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+// 237 Enumerable Magic #1 - True for All?- lvl 8
+// Description:
+// Task
+// Create a method all which takes two params:
+// 
+// a sequence
+// a function (function pointer in C)
+// and returns true if the function in the params returns true for every element in the sequence. Otherwise, it should return false. If the sequence is empty, it should return true, since technically nothing failed the test.
+// 
+// Example
+// all((1, 2, 3, 4, 5), greater_than_9) -> false
+// all((1, 2, 3, 4, 5), less_than_9)    -> True
+// Help
+// Here's a (Ruby) resource if you get stuck:
+function all( arr, fun ){
+  return arr.every(fun)
+}
 // 236: Smallest value of an array - lvl 7
 // Write a function that can return the smallest value of an array or the index of that value. The function's 2nd parameter will tell whether it should return the value or the index.
 // 
