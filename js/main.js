3188a3ecf9ab2980c3664e914cf030e96d82ad5e
Christopher Ament <chrisament45@gmail.com>
2025-06-08 18:13:38 -0400

    added 244: ES6 string addition - lvl 8 kata solutions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// 244: ES6 string addition - lvl 8
+// It is easy to join two strings together like this string1 + string2.
+// 
+// Another way to get the desired result would be with string1.concat(string2)
+// 
+// ES6 has introduced another way of joining strings. Your task is to find out what this is and write a function that will add two strings together, there must be a space between the two strings.
+// 
+// + , .concat() & .join() will not be allowed in this exercise. I have also removed some other methods that can be used to cheat!If one of the arguments is a number your code must coerce it into being a string.
+// 
+function joinStrings(string1, string2){
+  return `${string1} ${string2}`;
+ }
 // 243: Ordered Count of Characters - lvl 7
 // Description:
 // Count the number of occurrences of each character and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
