3edcb6d8bc71a9464bf4e43bc3e0af6575192f8b
Christopher Ament <chrisament45@gmail.com>
2025-06-16 14:23:55 -0400

    added  250: Alphabet symmetry - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+// 250: Alphabet symmetry - lvl 7
+// Description:
+// Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+// 
+// Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+// 
+// solve(["abode","ABc","xyzD"]) = [4, 3, 1]
+// See test cases for more examples.
+// 
+// Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
+// 
+// Good luck!
+function solve(arr){  
+  var alphabeth = "abcdefghijklmnopqrstuvwxyz";
+  return arr.map(x => x.toLowerCase().split('').filter((y,i) => i==alphabeth.indexOf(y)).length);
+};
 // 249: ASCII Total - lvl 8
 // Description:
 // You'll be given a string, and have to return the sum of all characters as an int. The function should be able to handle all printable ASCII characters.
