62d8907f0e576c8544b09757f670bc570adc21e8
Christopher Ament <chrisament45@gmail.com>
2025-06-22 16:06:56 -0400

    added 254: Area of a Square - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+// 254: Area of a Square - lvl 8
+// Description:
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+function squareArea(A){ 
+  let radius = (2 * A) / Math.PI;
+  let area = radius ** 2;
+  return Math.round(area * 100) / 100;
+  }
 // 253: Training JS #8: Conditional statement--switch - lvl 8
 function howManydays(month){
   switch (month){
