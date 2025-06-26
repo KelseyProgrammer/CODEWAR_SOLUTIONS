293fa040bea6518f12900a05ba098d87ed52dbca
Christopher Ament <chrisament45@gmail.com>
2025-06-26 15:30:40 -0400

    added 255: Sort the Gift Code - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+// 255: Sort the Gift Code - lvl 7
+// Description:
+// Happy Holidays fellow Code Warriors!
+// Santa's senior gift organizer Elf developed a way to represent up to 26 gifts by assigning a unique alphabetical character to each gift. After each gift was assigned a character, the gift organizer Elf then joined the characters to form the gift ordering code.
+// Santa asked his organizer to order the characters in alphabetical order, but the Elf fell asleep from consuming too much hot chocolate and candy canes! Can you help him out?
+function sortGiftCode(code) {
+  return code.split('').sort().join('');
+}
+Sort the Gift Code
+Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
 // 254: Area of a Square - lvl 8
 // Description:
 // Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
