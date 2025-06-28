d2deaaee1feff08dde46b9d51ff8ba183c3c0845
Christopher Ament <chrisament45@gmail.com>
2025-06-27 23:04:35 -0400

    added 256: Is there a vowel in there? - lvl 8 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+// 256: Is there a vowel in there? - lvl 8
+// Description:
+// Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+// 
+// If they are, change the array value to a string of that vowel.
+// 
+// input [100,100,116,105,117,121]=>[100,100,116,"i","u",121] output Return the resulting array.
+function isVow(arr) {
+  return arr.map(num => ({
+    97: 'a', 101: 'e', 105: 'i', 111: 'o', 117: 'u'
+  }[num] || num));
+}
 // 255: Sort the Gift Code - lvl 7
 // Description:
 // Happy Holidays fellow Code Warriors!
