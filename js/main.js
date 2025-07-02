e11a4191807ac890dd1a3a64041165073e3b45a6
Christopher Ament <chrisament45@gmail.com>
2025-07-01 21:26:14 -0400

    added 258: Sort by Last Char - lvl 7 kata solution

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// 258: Sort by Last Char - lvl 7
+// Description:
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+// 
+// If two words have the same last letter, the returned array should show them in the order they appeared in the given string.
+// 
+// All inputs will be valid.
+function last(x) {
+  return x.split(' ').sort((a, b) => {
+    const lastA = a[a.length - 1] || '';
+    const lastB = b[b.length - 1] || '';
+    return lastA.localeCompare(lastB);
+  });
+}
 // 257: Color Ghost - lvl 8
 // Description:
 // Color Ghost
